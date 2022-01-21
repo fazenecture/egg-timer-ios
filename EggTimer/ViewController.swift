5d775ddcaaf99b6f933a5a838af748738c97ecd4
Vivek <vivekkumarsingh2002@gmail.com>
2022-01-22 03:01:37 +0530

    final-push

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -1,20 +1,28 @@
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
     
     let eggTimes = ["Soft":3, "Medium":4, "Hard":7]
     
-    var secondsRemaning = 60
+    var totalTime = 0
+    var secondsPassed = 0
     var timer = Timer()
+    var player: AVAudioPlayer!
     
     @IBAction func hardnessSelector(_ sender: UIButton) {
         let hardness = sender.currentTitle!
         
+        progressBar.progress = 0.0
+        totalTime = 0
+        secondsPassed = 0
+        titleLabel.text = hardness
+        
         timer.invalidate()
         
-        secondsRemaning = eggTimes[hardness]!
-        print(secondsRemaning)
+        totalTime = eggTimes[hardness]!
+//        print(secondsRemaning)
         
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
@@ -22,12 +30,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     
     @objc func updateTimer(){
-        if secondsRemaning > 0 {
-            print("\(secondsRemaning) seconds")
-            secondsRemaning -= 1
+        if secondsPassed <= totalTime {
+            secondsPassed += 1
+            let percentageProgress = Float(secondsPassed) / Float(totalTime)
+
+            progressBar.progress = Float(percentageProgress)
+//            print("\(secondsRemaning) seconds")
+
         }else{
             timer.invalidate()
-            titleLabel.text = "Done"
+            titleLabel.text = "Done!"
+            playSound()
         }
     }
+    
+    func playSound(){
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: ".mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    
 }
